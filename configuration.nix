b0117044c81c1d685bdcc7a9e68bab7668fe9005
Simon <simon@aquila.earth>
2025-09-19 16:47:08 +1000

    feat: add home-manager

--- configuration.nix
+++ configuration.nix
@@ -2,12 +2,13 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.home-manager
     ];
 
   # Bootloader.
@@ -96,6 +97,11 @@
     packages = with pkgs; [
     #  thunderbird
     ];
+  };
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users.simon = import ./home.nix;
   };
 
   # Install firefox.
